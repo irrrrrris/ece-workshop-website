0aae22bd85598bf53baae55fe4c2eef805ed2a60
ShuntaroWakamatsu <123689445+ShuntaroWakamatsu@users.noreply.github.com>
2023-07-07 17:07:55 -0400

    Software.js Software.css Hardware.js Hardware.css are done

--- src/components/Hardware.js
+++ src/components/Hardware.js
@@ -5,8 +5,17 @@ const Hardware = () => {
   return (
     <div className='hardware'>
         <div className='content'>
-          <h1>2023 Summer</h1>
-          <h1>2022 Summer</h1>
+        <h1>ECE Summer Hardware Workshops</h1>
+          <p className='description'>In our hardware workshops, we provide a quick review of the hardware course, ECE110 and a hands-on experience 
+            of the advanced hardware knowledge that students learn in their 2nd year courses such as <a href='https://engineering.calendar.utoronto.ca/course/ece212h1' className='highlight'>ECE212(Circuit Analysis)</a>
+             and <a href='https://engineering.calendar.utoronto.ca/course/ece231h1' className='highlight'>ECE231(Introductory Electronics).</a> Students will have a quick lecture on new topics followed by lab exercises
+             in a team, which enhance not only the understanding of the topics but also the communication skills, research skills, and debugging skills.
+          </p>
+          <h2>2023 Summer</h2>
+            <a className='link' href='/hardware2023'>Hardware Workshop 1 (Nicholas Mutlak)</a>
+            <a className='link' href='/microbotics'>Hardware Workshop 2 - Microbotics - (Ryan Seto)</a>
+          <h2>2022 Summer</h2>
+            <a className='link' href='/hardware2022'>Hardware Workshop</a>
         </div>
     </div>
   )
--- src/components/Software.js
+++ src/components/Software.js
@@ -5,8 +5,19 @@ const Software = () => {
   return (
     <div className='software'>
         <div className='content'>
-          <h1>2023 Summer</h1>
-          <h1>2022 Summer</h1>
+          <h1>ECE Summer Software Workshops</h1>
+          <p className='description'>
+            In our software workshops, we provide a thorough review of the C language course, APS105 and 
+            a brief introduction into the 2nd year programming courses such as <a href='https://engineering.calendar.utoronto.ca/course/ece244h1' className='highlight'>ECE244(Programming Fundamentals) </a> 
+            and <a href='https://engineering.calendar.utoronto.ca/course/ece297h1' className='highlight'>ECE297(Software Design and Communication)</a>. 
+            Students will have a lecture on C and C++ followed by a short practice questions. At the end of the workshop, they will work on a project
+            to put what they learnt in practice.
+          </p>
+          <h2>2023 Summer</h2>
+            <a className='link' href='/software2023'>Software Workshop - C & C++ - (Iris, Simon, Shuntaro)</a>
+          <h2>2022 Summer</h2>
+            <a className='link' href='/software2022'>Software Workshop</a>
+            <a className='link' href='/pythonML'>Machine Learning & Python</a>
         </div>
     </div>
   )
