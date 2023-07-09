c8d4aa29e0b75d44579e3e3b3171a50cf796bc25
ShuntaroWakamatsu <123689445+ShuntaroWakamatsu@users.noreply.github.com>
2023-07-09 19:27:31 -0400

    Software2023 is done

--- src/components/software2023.js
+++ src/components/software2023.js
@@ -1,11 +1,60 @@
 import React from 'react';
 import './Software2023.css';
+import PDF from './files/SoftwareWorkshop.pdf';
+import PPTX from './files/SoftwareWorkshop.pptx'
 
 const Software2023 = () => {
   return (
     <div className='software2023'>
         <div className='content'>
-            <h1>Hello World</h1>
+            <h1>2023 Summer Software Workshop</h1>
+            <p>In this workshop, we reviewed APS105 and introduced some concepts from ECE244 and ECE297</p>
+            <div className='labelContainer'>
+              <h4>C</h4>
+              <h4></h4>
+              <h4>C++</h4>
+            </div>
+            <div className='listContainer'>
+              <ul className='list'>
+                <li>Loop</li>
+                <li>Array</li>
+                <li>String</li>
+                <li>Pointer</li>
+                <li>Function</li>
+                <li>Random Number</li>
+                <li>Recursion</li>
+              </ul>
+              <ul className='list'>
+                <li>Header Files</li>
+                <li>Dynamic Memory</li>
+                <li>Struct</li>
+                <li>Linked List</li>
+                <li>Sort Algorithm</li>
+                <li>Binary Search Tree</li>
+                <li>Hash Table</li>
+              </ul>
+              <ul className='list'>
+                <li>IO Stream</li>
+                <li>String Stream</li>
+                <li>Object Oriented</li>
+                <li>Class</li>
+                <li>Encapsulation</li>
+                <li>Constructor</li>
+                <li>Destructor</li>
+              </ul>
+              <ul className='list'>
+                <li>Copy Constructor</li>
+                <li>Operator Overloading</li>
+                <li>Standard Template Library</li>
+                <li>Templates</li>
+                <li>Inheritance</li>
+              </ul>
+            </div>
+            <br></br>
+            <p id='Download'>Download Lecture Slides
+            <a className='dl' href={PDF} download>PDF Format</a>
+            <a className='dl' href={PPTX} download>MS PowerPoint Format</a>
+            </p>
         </div>
     </div>
   )
