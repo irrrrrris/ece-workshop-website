a6173f3a36d6f166f397e124232c84e839b1807d
ShuntaroWakamatsu <123689445+ShuntaroWakamatsu@users.noreply.github.com>
2023-07-10 02:50:55 -0400

    hardware2022 is done

--- src/components/hardware2022.js
+++ src/components/hardware2022.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import './Hardware2022.css'
+import Hardware_1 from './images/Hardware_1.jpg';
+import Hardware_2 from './images/Hardware_2.jpg';
 
 const Hardware2022 = () => {
   return (
     <div className='hardware2022'>
         <div className='content'>
-            
+          <h1>2022 Summer Software Workshop</h1>
+          <p>In this workshop, we reviewed APS105</p>
+          <div className='container'>
+            <img className='image1' src={Hardware_1} alt='image1'></img>
+            <img className='image2' src={Hardware_2} alt='image2'></img>
+          </div>
         </div>
     </div>
   )
--- src/components/hardware2023.js
+++ src/components/hardware2023.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import './Hardware2023.css'
+import Hardware_1 from './images/Hardware_1.jpg';
+import Hardware_2 from './images/Hardware_2.jpg';
 
 const Hardware2023 = () => {
   return (
     <div className='hardware2023'>
         <div className='content'>
-          <div className='aaa'>
-            Hello
+          <h1>2022 Summer Software Workshop</h1>
+          <p>In this workshop, we reviewed APS105</p>
+          <div className='container'>
+            <img className='image1' src={Hardware_1} alt='image1'></img>
+            <img className='image2' src={Hardware_2} alt='image2'></img>
           </div>
         </div>
     </div>
