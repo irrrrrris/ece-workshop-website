6fe63c476b8c3ae925f7f0e51a637950d0258371
ShuntaroWakamatsu <123689445+ShuntaroWakamatsu@users.noreply.github.com>
2023-07-10 02:08:00 -0400

    Software2022 is done

--- src/components/software2022.js
+++ src/components/software2022.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import './Software2022.css'
+import Software_1 from './images/Software_1.jpg';
+import Software_2 from './images/Software_2.jpg';
 
 const Software2022 = () => {
   return (
     <div className='software2022'>
         <div className='content'>
-
+          <h1>2022 Summer Software Workshop</h1>
+          <p>In this workshop, we reviewed APS105</p>
+          <div className='container'>
+            <img className='image1' src={Software_1} alt='image1'></img>
+            <img className='image2' src={Software_2} alt='image2'></img>
+          </div>
         </div>
     </div>
   )
